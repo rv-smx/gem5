26bba95d92996f31cedb731311c57a60f7b8b9fb
MaxXSoft <x@MaxXSoft.net>
2023-02-28 15:20:35 +0800

    arch-riscv: Add stream engine for SMX extension.

---
+++ src/arch/riscv/stream_engine.cc
@@ -0,0 +1,293 @@
+/*
+ * Copyright (c) 2023 Max Xing
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "arch/riscv/stream_engine.hh"
+
+#include <cassert>
+#include <cstdint>
+
+#include "arch/riscv/regs/int.hh"
+#include "base/bitfield.hh"
+#include "base/compiler.hh"
+#include "cpu/thread_context.hh"
+#include "debug/StreamEngine.hh"
+
+namespace
+{
+
+constexpr unsigned MAX_INDVAR_NUM = 4;
+constexpr unsigned MAX_MEMORY_NUM = 32;
+constexpr unsigned MAX_ADDR_NUM = 4;
+
+gem5::RegVal
+applyWidthUnsigned(gem5::RegVal val, unsigned width, bool is_unsigned)
+{
+    switch (width) {
+      case 0b00:
+        return is_unsigned ? gem5::bits(val, 7, 0) : gem5::szext<8>(val);
+      case 0b01:
+        return is_unsigned ? gem5::bits(val, 15, 0) : gem5::szext<16>(val);
+      case 0b10:
+        return is_unsigned ? gem5::bits(val, 31, 0) : gem5::szext<32>(val);
+      case 0b11:
+        return val;
+      default:
+        GEM5_UNREACHABLE;
+    }
+}
+
+} // namespace
+
+namespace gem5
+{
+
+namespace RiscvISA
+{
+
+bool
+StreamEngine::addAddrConfigForLastMem(RegVal stride, unsigned dep,
+        SmxStreamKind kind)
+{
+    if (!isValidStream(dep, kind)) return false;
+    auto &addrs = mems.back().addrs;
+    unsigned memory_id = mems.size() - 1;
+
+    if (addrs.size() >= MAX_ADDR_NUM) {
+        DPRINTF(StreamEngine,
+            "Address factor number of memory stream %u exceeded! "
+            "Currently supports %u address factors\n",
+            memory_id, MAX_ADDR_NUM);
+        return false;
+    }
+    if (kind == SMX_KIND_MS) {
+        DPRINTF(StreamEngine,
+            "Try to configure indirect memory acess for memory stream %u! "
+            "Currently does not support indirect memory access\n",
+            memory_id);
+        return false;
+    }
+
+    addrs.push_back({stride, dep, kind});
+    if (kind == SMX_KIND_IV) {
+        ivs[dep].users.push_back(mems.size() - 1);
+    } else {
+        mems[dep].users.push_back(mems.size() - 1);
+    }
+    return true;
+}
+
+RegVal
+StreamEngine::getIndvarReg(ThreadContext *tc, unsigned indvar_id)
+{
+    switch (indvar_id) {
+      case 0:
+        return tc->getReg(IndVar0Reg);
+      case 1:
+        return tc->getReg(IndVar1Reg);
+      case 2:
+        return tc->getReg(IndVar2Reg);
+      case 3:
+        return tc->getReg(IndVar3Reg);
+      default:
+        GEM5_UNREACHABLE;
+    }
+}
+
+void
+StreamEngine::setIndvarReg(ThreadContext *tc, unsigned indvar_id,
+        RegVal value)
+{
+    switch (indvar_id) {
+      case 0:
+        return tc->setReg(IndVar0Reg, value);
+      case 1:
+        return tc->setReg(IndVar1Reg, value);
+      case 2:
+        return tc->setReg(IndVar2Reg, value);
+      case 3:
+        return tc->setReg(IndVar3Reg, value);
+      default:
+        GEM5_UNREACHABLE;
+    }
+}
+
+void
+StreamEngine::clear()
+{
+    ivs.clear();
+    mems.clear();
+}
+
+bool
+StreamEngine::addIndvarConfig(RegVal _init_val, RegVal _step_val,
+        RegVal _final_val, SmxStopCond cond, unsigned width, bool is_unsigned)
+{
+    if (ivs.size() >= MAX_INDVAR_NUM) {
+        DPRINTF(StreamEngine, "Induction variable stream number exceeded! "
+            "Currently supports %u induction variable streams\n",
+            MAX_INDVAR_NUM);
+        return false;
+    }
+    if (cond < SMX_COND_GT || cond > SMX_COND_NE) {
+        DPRINTF(StreamEngine, "Unsupported stop condition %u "
+            "for induction variable stream %u\n", cond, (unsigned)ivs.size());
+        return false;
+    }
+
+    auto init_val = applyWidthUnsigned(_init_val, width, is_unsigned);
+    auto step_val = applyWidthUnsigned(_step_val, width, is_unsigned);
+    auto final_val = applyWidthUnsigned(_final_val, width, is_unsigned);
+    ivs.push_back(
+        {init_val, step_val, final_val, cond, width, is_unsigned, {}});
+    return true;
+}
+
+bool
+StreamEngine::addMemoryConfig(RegVal base, RegVal stride1, unsigned dep1,
+        SmxStreamKind kind1, bool prefetch, unsigned width)
+{
+    if (mems.size() >= MAX_MEMORY_NUM) {
+        DPRINTF(StreamEngine, "Memory stream number exceeded! "
+            "Currently supports %u memory streams\n",
+            MAX_MEMORY_NUM);
+        return false;
+    }
+    mems.push_back({base, prefetch, width, {}, {}});
+    return addAddrConfigForLastMem(stride1, dep1, kind1);
+}
+
+bool
+StreamEngine::addAddrConfig(
+        RegVal stride1, unsigned dep1, SmxStreamKind kind1,
+        RegVal stride2, unsigned dep2, SmxStreamKind kind2)
+{
+    return addAddrConfigForLastMem(stride1, dep1, kind1) &&
+            addAddrConfigForLastMem(stride2, dep2, kind2);
+}
+
+bool
+StreamEngine::ready(ThreadContext *tc)
+{
+    if (ivs.empty()) {
+        DPRINTF(StreamEngine, "No induction variable stream configured\n");
+        return false;
+    }
+    for (unsigned i = 0; i < ivs.size(); ++i) {
+        setIndvarReg(tc, i, ivs[i].initVal);
+    }
+    return true;
+}
+
+bool
+StreamEngine::end()
+{
+    clear();
+    return true;
+}
+
+bool
+StreamEngine::step(ThreadContext *tc, unsigned indvar_id)
+{
+    if (!isValidStream(indvar_id, SMX_KIND_IV)) return false;
+    auto &iv = ivs[indvar_id];
+    auto value = getIndvarReg(tc, indvar_id) + iv.stepVal;
+    setIndvarReg(tc, indvar_id,
+        applyWidthUnsigned(value, iv.width, iv.isUnsigned));
+    for (; indvar_id < ivs.size(); ++indvar_id) {
+        setIndvarReg(tc, indvar_id, ivs[indvar_id].initVal);
+    }
+    return true;
+}
+
+bool
+StreamEngine::isValidStream(unsigned id, SmxStreamKind kind) const
+{
+    if (kind == SMX_KIND_IV) {
+        if (id >= ivs.size()) {
+            DPRINTF(StreamEngine, "Invalid induction variable stream %u\n",
+                id);
+            return false;
+        }
+        return true;
+    } else if (kind == SMX_KIND_MS) {
+        if (id >= mems.size()) {
+            DPRINTF(StreamEngine, "Invalid memory stream %u\n", id);
+            return false;
+        }
+        return true;
+    } else {
+        DPRINTF(StreamEngine, "Unsupported stream kind %u\n", kind);
+        return false;
+    }
+}
+
+Addr
+StreamEngine::getMemoryAddr(ThreadContext *tc, unsigned memory_id) const
+{
+    const auto &mem = mems[memory_id];
+    Addr vaddr = mem.base;
+    for (const auto &addr : mem.addrs) {
+        assert(addr.kind == SMX_KIND_IV);
+        vaddr += getIndvarReg(tc, addr.dep) * addr.stride;
+    }
+    return vaddr;
+}
+
+bool
+StreamEngine::isNotInLoop(ThreadContext *tc, unsigned indvar_id) const
+{
+    const auto &iv = ivs[indvar_id];
+    auto value = getIndvarReg(tc, indvar_id);
+    switch (iv.cond) {
+      case SMX_COND_GT:
+        return (int64_t)value > (ino64_t)iv.finalVal;
+      case SMX_COND_GE:
+        return (int64_t)value >= (ino64_t)iv.finalVal;
+      case SMX_COND_LT:
+        return (int64_t)value < (ino64_t)iv.finalVal;
+      case SMX_COND_LE:
+        return (int64_t)value <= (ino64_t)iv.finalVal;
+      case SMX_COND_GTU:
+        return value > iv.finalVal;
+      case SMX_COND_GEU:
+        return value >= iv.finalVal;
+      case SMX_COND_LTU:
+        return value < iv.finalVal;
+      case SMX_COND_LEU:
+        return value <= iv.finalVal;
+      case SMX_COND_EQ:
+        return value == iv.finalVal;
+      case SMX_COND_NE:
+        return value != iv.finalVal;
+      default:
+        GEM5_UNREACHABLE;
+    }
+}
+
+} // namespace RiscvISA
+} // namespace gem5
---
+++ src/arch/riscv/stream_engine.hh
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2023 Max Xing
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __ARCH_RISCV_STREAM_ENGINE_HH__
+#define __ARCH_RISCV_STREAM_ENGINE_HH__
+
+#include <vector>
+
+#include "base/types.hh"
+
+namespace gem5
+{
+
+class ThreadContext;
+
+namespace RiscvISA
+{
+
+enum SmxStopCond
+{
+  SMX_COND_GT = 0,
+  SMX_COND_GE = 1,
+  SMX_COND_LT = 2,
+  SMX_COND_LE = 3,
+  SMX_COND_GTU = 4,
+  SMX_COND_GEU = 5,
+  SMX_COND_LTU = 6,
+  SMX_COND_LEU = 7,
+  SMX_COND_EQ = 8,
+  SMX_COND_NE = 9,
+};
+
+enum SmxStreamKind
+{
+  SMX_KIND_IV = 0,
+  SMX_KIND_MS = 1,
+};
+
+/**
+ * Stream engine of the SMX ISA extension.
+ *
+ * @todo Support prefetch.
+ */
+class StreamEngine
+{
+  private:
+    struct IndvarConfig
+    {
+        RegVal initVal;
+        RegVal stepVal;
+        RegVal finalVal;
+        SmxStopCond cond;
+        unsigned width;
+        bool isUnsigned;
+        std::vector<unsigned> users;
+    };
+
+    struct AddrConfig
+    {
+        RegVal stride;
+        unsigned dep;
+        SmxStreamKind kind;
+    };
+
+    struct MemoryConfig
+    {
+        RegVal base;
+        bool prefetch;
+        unsigned width;
+        std::vector<AddrConfig> addrs;
+        std::vector<unsigned> users;
+    };
+
+    std::vector<IndvarConfig> ivs;
+    std::vector<MemoryConfig> mems;
+
+    bool addAddrConfigForLastMem(RegVal stride, unsigned dep,
+            SmxStreamKind kind);
+
+    RegVal getIndvarReg(ThreadContext *tc, unsigned indvar_id);
+
+    void setIndvarReg(ThreadContext *tc, unsigned indvar_id, RegVal value);
+
+  public:
+    StreamEngine()
+    {
+        clear();
+    }
+
+    void clear();
+
+    bool addIndvarConfig(RegVal init_val, RegVal step_val, RegVal final_val,
+            SmxStopCond cond, unsigned width, bool is_unsigned);
+
+    bool addMemoryConfig(RegVal base, RegVal stride1, unsigned dep1,
+            SmxStreamKind kind1, bool prefetch, unsigned width);
+
+    bool addAddrConfig(RegVal stride1, unsigned dep1, SmxStreamKind kind1,
+            RegVal stride2, unsigned dep2, SmxStreamKind kind2);
+
+    bool ready(ThreadContext *tc);
+
+    bool end();
+
+    bool step(ThreadContext *tc, unsigned indvar_id);
+
+    bool isValidStream(unsigned id, SmxStreamKind kind) const;
+
+    Addr getMemoryAddr(ThreadContext *tc, unsigned memory_id) const;
+
+    bool isNotInLoop(ThreadContext *tc, unsigned indvar_id) const;
+};
+
+} // namespace RiscvISA
+} // namespace gem5
+
+#endif // __ARCH_RISCV_STREAM_ENGINE_HH__
